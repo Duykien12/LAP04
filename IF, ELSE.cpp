2364d025bfcc1863325c86c169d905a29750659f
Duykien12 <71129886+Duykien12@users.noreply.github.com>
2020-09-16 17:48:52 +0700

    commit

---
+++ IF, ELSE.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+int a,b,c;
+
+	printf (" Enter the frist number\n ");
+	scanf ("%d", &a);
+	printf (" Enter the second number\n ");
+	scanf ("%d", &b);
+	c = a + b;
+	printf ( " %d + %d = %d",a,b,c);
+	return 0;
+}
+
+
